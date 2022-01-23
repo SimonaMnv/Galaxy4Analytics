e1f6ce49a085cf884088c2975caa8db0870c4766
SimonaMnv <simona.mnv@gmail.com>
2022-01-23 14:30:01 +0200

    updt tasks

--- tasks.py
+++ tasks.py
@@ -139,7 +139,7 @@ def lint(ctx):
     ctx.run('flake8')
 
 
-@task(grant_pg_db, lint, test_unit)
+@task(grant_pg_db, lint)
 def ci(ctx):
     """
     Run all the applicable tests that our CI process runs.
