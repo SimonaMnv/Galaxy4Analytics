2736d36d7bfce6cf7b89d7b97ff32c8d92735685
SimonaMnv <simona.mnv@gmail.com>
2022-02-25 19:43:14 +0200

    rm missing lines from coverage

--- tasks.py
+++ tasks.py
@@ -122,8 +122,7 @@ def test_coverage(ctx):
     test coverage
     """
     print('Running test coverage...')
-    shell.command_no_suppress('coverage run --branch -m unittest tests/gdrive_to_local_dag_test.py && coverage report \
-                              -m')
+    shell.command_no_suppress('coverage run --branch -m unittest tests/gdrive_to_local_dag_test.py && coverage report')
 
 
 @task
