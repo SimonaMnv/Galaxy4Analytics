5b89cc6d3a96adad47fa0853506f0f3b3063dce8
SimonaMnv <simona.mnv@gmail.com>
2022-03-26 15:06:52 +0200

    dev -> prod

--- dags/custom_packages/config_vars.py
+++ dags/custom_packages/config_vars.py
@@ -1,7 +1,7 @@
 import os
 
 config = {
-    "ENV": "dev",
+    "ENV": "prod",
     "project_root": os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
     "database_url": os.environ.get("DATABASE_URL")
 }
